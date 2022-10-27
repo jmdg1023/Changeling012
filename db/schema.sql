ece4c8540f395beffdbcc649e56eb4f7f384fddb
Astrid <jmdg1023@gmail.com>
2022-10-27 18:11:56 +1100

    debugging

--- db/schema.sql
+++ db/schema.sql
@@ -2,7 +2,7 @@ DROP DATABASE IF EXISTS employee_tracker;
 CREATE DATABASE employee_tracker;
 USE employee_tracker;
 
-CREATE TABLE department(
+CREATE TABLE departments(
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL 
 );
